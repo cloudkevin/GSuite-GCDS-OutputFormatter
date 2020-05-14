d3723a744956b189154585a9988e6739501a4d76
cloudkevin <cloudkevin@users.noreply.github.com>
2020-05-14 13:25:59 -0600

    Update gcdsCleanup.py

--- gcdsCleanup.py
+++ gcdsCleanup.py
@@ -3,18 +3,16 @@ inputfile = open('<FILE_NAME>', encoding = "ISO-8859-1")
 outputfile = open('gcdsCleanedOutput.csv', 'w')
 regex = re.compile('"([^"]*)"')
 text = inputfile.readlines()
+writer = csv.writer(outputfile, delimiter=' ')
 
 print('\n[+] Opening csv file...')
 
-writer = csv.writer(outputfile, delimiter=' ')
 for line in text:
-	line = line.replace('\x00', '')
-	line = line.replace('\n', '')
-	if len(line) > 0:
-		if 'New' in line:
-			value = re.findall(r'"([^"]*)"', line)[0]
-			if value.startswith('[') and value.endswith(']'):
-				value = value[1:len(value)-1]
-			writer.writerow([value])
+	line = line.replace('\x00', '').replace('\n', '')
+	if len(line) > 0 and 'New' in line:
+		value = re.findall(r'"([^"]*)"', line)[0]
+		if value.startswith('[') and value.endswith(']'):
+			value = value[1:len(value)-1]
+		writer.writerow([value])
 
 print('\n[+] Finished Processing\n')
